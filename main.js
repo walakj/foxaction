87af248e05062f11b95c85319bc0225f54ca927f
Local Fox <walakj@users.noreply.github.com>
2023-05-03 20:12:29 +0200

    more debugging

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const exec = require('@actions/exec');
 
 
 try {
-  core.notice("starting foxaction!")
+  core.notice("starting the new foxaction!")
 
   // Get the message input from the action metadata file
   const message = core.getInput('message');
   // Print the message to the standard output
-  console.log(message);
-  
+  console.log("console.log: the message is " + message);
+  core.notice("core.notice: the message is " + message);
   
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
